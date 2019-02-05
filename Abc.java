98b2e94c843033b18d0de787c83268d1a9ee514b
AyushiSH <47345710+AyushiSH@users.noreply.github.com>
2019-02-05 12:48:04 +0530

    Add files via upload

---
+++ Abc.java
@@ -0,0 +1,12 @@
+import java.util.scanner*;
+Class Abc
+{
+public static void main(String args[])
+{
+scanner sc=new Scanner(system.in);
+int a=sc.nextint();
+int b=sc.nextint();
+int c=a+b;
+System.out.println("The Sum of A and B is "c);
+}
+}
